42153e43d6bb54448a8bfff1c0c56ecb156d1f43
it7dog <wangwei@lichendt.com>
2018-03-21 10:42:55 +0800

    FileSource second commit

--- chapter04/src/main/java/com/smart/resource/FileSourceExample.java
+++ chapter04/src/main/java/com/smart/resource/FileSourceExample.java
@@ -15,14 +15,15 @@ public class FileSourceExample {
     public static void main(String[] args) throws IOException {
         String filePath = "/Users/wangw/Documents/github/masterSpring/chapter04/src/main/resources/conf/file1.txt";
 
+        //使用系统文件路径方式加载问价
         WritableResource res1 = new PathResource(filePath);
-
+        //使用类路径方式加载文件
         Resource res2 = new ClassPathResource("conf/file1.txt");
-
+        //使用WritableResource接口写资源文件
         OutputStream stream1 = res1.getOutputStream();
         stream1.write("欢迎光临\n小春论坛".getBytes());
         stream1.close();
-
+        //使用Resource接口读资源文件
         InputStream ins1 = res1.getInputStream();
         InputStream ins2 = res2.getInputStream();
 
