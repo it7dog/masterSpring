f3d2de5e91c677d20f941851a91ffb57c4eee9ad
王炜 <wangwei@lichendt.com>
2018-05-03 21:44:33 +0800

    aop 概念

---
+++ aopDemo/src/main/java/com/itcast/c_proxy/UserServiceProxyCglibFactory.java
@@ -0,0 +1,38 @@
+package com.itcast.c_proxy;
+
+import com.itcast.service.UserService;
+import com.itcast.service.impl.UserServiceImpl;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class UserServiceProxyCglibFactory implements MethodInterceptor {
+
+    public UserService getUserServiceProxy(){
+        Enhancer en = new Enhancer();  //帮我们生成代理对象
+
+        en.setSuperclass(UserServiceImpl.class); //设置对谁进行代理
+
+        en.setCallback(this); //代理要干什么
+
+        UserService us = (UserService) en.create(); //创建代理对象
+
+        return us;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+
+        //打开事务
+        System.out.println("打开事务");
+        //调用原有方法
+        Object returnValue = methodProxy.invokeSuper(o,objects);
+        //提交事务
+        System.out.println("提交事务");
+
+        return returnValue;
+    }
+}
--- aopDemo/src/test/java/com/itcast/c_proxy/Demo.java
+++ aopDemo/src/test/java/com/itcast/c_proxy/Demo.java
@@ -14,4 +14,17 @@ public class Demo {
 
         usProxy.save();
     }
+    @Test
+    public void fun2(){
+
+        UserServiceProxyCglibFactory factory = new UserServiceProxyCglibFactory();
+
+        UserService usProxy = factory.getUserServiceProxy();
+
+        usProxy.save();
+
+        //判断代理对象是否属于被代理对象类型
+        //代理对象继承了被代理对象
+        System.out.println(usProxy instanceof UserServiceImpl);
+    }
 }
